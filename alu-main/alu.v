993782b8eaf16fa81e2a8ae99cf2f178bdeb38f6
JakeVigeantProfessional <jmv43duke.edu>
2023-02-10 05:34:41 -0500

    right barrel shifter

--- alu-main/alu.v
+++ alu-main/alu.v
@@ -34,5 +34,5 @@ module alu(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 
     // use 8 bit mux to select output based on opcode
     //mux_8(in0,in1,in2,in3,in4,in5,in6,in7, out, select);
-    mux_8 resultSelection(addRes, addRes, andRes, orRes, sllRes, sraRes);
+    //mux_8 resultSelection(addRes, addRes, andRes, orRes, sllRes, sraRes);
 endmodule
