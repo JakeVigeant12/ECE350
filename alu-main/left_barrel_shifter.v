e9ab7a5a11454f04145e9b09d1a6dec49cc19e68
JakeVigeantProfessional <jmv43duke.edu>
2023-02-10 05:30:43 -0500

    left barrel shift

--- alu-main/left_barrel_shifter.v
+++ alu-main/left_barrel_shifter.v
@@ -1,13 +1,27 @@
 module left_barrel_shifter (data, amt, out);
-    input [32:0] data;
-    input amt [4:0];
-    output [32:0] out;
+    input [31:0] data;
+    input [4:0] amt;
+    output [31:0] out;
 
-    //for each mux, input both the shifted and unshifted versions, let amt act as select bits to choose
+    wire [31:0] w1,w2,w3,w4, s1, s2,s3,s4,s5;
+
+    //for each mux, input previous level's result, let amt act as select bits to choose
     //if this shift occurs
-    assign out[31:1] = x[30:0];
+    //module mux_2(out, select, in0, in1);
+    one_bit_left_shift shift1(data, s1);
+    mux_2 level1(w1,amt[0],data, s1);
 
-    assign out[0] = 1'b0; 
+    two_bit_left_shift shift2(w1, s2);
+    mux_2 level2(w2,amt[1],w1, s2);
+
+    four_bit_left_shift shift3(w2, s3);
+    mux_2 level3(w3,amt[2],w2, s3);
+
+    eight_bit_left_shift shift4(w3, s4);
+    mux_2 level4(w4,amt[3],w3, s4);
+
+    sixteen_bit_left_shift shift5(w4, s5);
+    mux_2 level5(out,amt[4],w4, s5);
 
 
 endmodule
